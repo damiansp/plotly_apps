ebfd8faf1fb4ae362d071043635a09c6a332eff4
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2017-11-07 14:51:10 -0800

    more basic tutorials

--- tutorial/app.py
+++ tutorial/app.py
@@ -5,9 +5,9 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='Dash: A web application framework for Python'),
+app.layout = html.Div([
+    html.H1('Hello Dash'),
+    html.Div('Dash: A web application framework for Python'),
     dcc.Graph(
         id='example-graph',
         figure={
---
+++ tutorial/markdown.py
@@ -0,0 +1,19 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash
+
+app = dash.Dash()
+
+markdown_text = '''
+### Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+'''
+
+app.layout = html.Div([dcc.Markdown(markdown_text)])
+
+if __name__ == '__main__':
+    app.run_server()
